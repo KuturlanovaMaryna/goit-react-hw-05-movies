c356cf108ac2796826fa7e59e7beaebd98451f09
KuturlanovaMaryna <129969564+KuturlanovaMaryna@users.noreply.github.com>
2023-11-09 15:22:18 +0200

    updete app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
+import { NavLink, Route, Routes } from 'react-router-dom';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <nav>
+        <NavLink>Home</NavLink>
+        <NavLink>Movies</NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<div>Home page</div>} />
+      </Routes>
     </div>
   );
 };
+
+// const API_KEY = 'b5c5f92c171eddbfa17290cd377a7ee0';
